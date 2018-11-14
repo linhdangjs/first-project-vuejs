1b72ab349e449120e6f9547b4f2c63d2b69a893d
linhdangjs <linhdang.js9896@gmail.com>
2018-11-15 01:34:59 +0700

    implement attack methods

--- app.js
+++ app.js
@@ -10,6 +10,45 @@ new Vue ({
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
+        },
+        attack: function() {
+            this.monsterHealth -= this.calculateDame(3, 10);
+
+            if(this.checkWin()) {
+                return;
+            }
+            this.playerHealth -= this.calculateDame(5, 12);
+            this.checkWin();
+        },
+        specialAttack: function() {
+
+        },
+        heal: function() {
+
+        },
+        giveUp: function() {
+
+        },
+        calculateDame: function(min, max) {
+            return Math.max(Math.floor(Math.random() * max) + 1, min);
+        },
+        checkWin: function() {
+            if(this.monsterHealth <= 0) {
+                if(confirm('You won! New Game?')) {
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+                return true;
+            } else if (this.playerHealth <=0) {
+                if(confirm('You lost! New Game?')) {
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+                return true;
+            }
+            return false;    
         }
     }
 })

124b21b21d15f17cbe588deb902052a5766296fc
linhdangjs <linhdang.js9896@gmail.com>
2018-11-15 17:43:37 +0700

    implement  heal method

--- app.js
+++ app.js
@@ -28,7 +28,12 @@ new Vue ({
             this.monsterAttack();
         },
         heal: function() {
-
+            if(this.playerHealth <= 90) {
+                this.playerHealth += 10;
+            } else {
+                this.playerHealth = 100;
+            }
+            this.monsterAttack();
         },
         giveUp: function() {
 
